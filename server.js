4aac5ee7db318bd87865bd5df2eb830ed38e8c86
Nick Hart <nick.hart.des@gmail.com>
2020-09-09 18:32:03 -0500

    Add server boilerplate

--- server.js
+++ server.js
@@ -1,1 +1,11 @@
-
+import express from 'express'
+import cors from 'cors'
+const app = express()
+app.use(express.json())
+app.use(cors())
+​
+app.set('port', process.env.PORT || 3005)
+​
+app.listen(app.get('port'), () => {
+  console.log(`This server is running on http://localhost:${app.get('port')}`)
+})
