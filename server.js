ff2668fd32ef7c55ce19a0bff554c82d26624e93
Nick Hart <nick.hart.des@gmail.com>
2020-09-09 18:37:38 -0500

    Fix server boilerplate

--- server.js
+++ server.js
@@ -3,9 +3,9 @@ import cors from 'cors'
 const app = express()
 app.use(express.json())
 app.use(cors())
-​
+
 app.set('port', process.env.PORT || 3005)
-​
+
 app.listen(app.get('port'), () => {
   console.log(`This server is running on http://localhost:${app.get('port')}`)
 })
