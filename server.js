1056ab5195bdfe654bf0b84f23f907fc828792ee
Greyson Elkins <greysonelkins@gmail.com>
2020-09-10 14:58:04 -0600

    Push to database (?)

--- server.js
+++ server.js
@@ -1,10 +1,34 @@
+require("dotenv").config();
+
 import express from 'express'
 import cors from 'cors'
+
 const app = express()
+const knex = require('knex')({
+  client: 'pg',
+  connection: `postgres://${process.env.DB_USER}:${process.env.DB_PASSWORD}@localhost/exquisite_data`
+})
+
 app.use(express.json())
 app.use(cors())
 
 app.set('port', process.env.PORT || 3005)
+
+app.post('/authors', async(request, response) => {
+  const author = request.body
+  const requiredKeys = ['name', 'email']
+  if (requiredKeys.every(value => Object.keys(author).includes(value))) {
+    try {
+      knex('authors').insert({ name: author.name, email: author.email })
+        .then((response) => console.log(response))
+    } catch (error) {
+      console.error(error.message)
+    }
+  } else {
+    //return an error message
+    response(422).json(`You don't got the right info`)
+  }
+})
 
 app.listen(app.get('port'), () => {
   console.log(`This server is running on http://localhost:${app.get('port')}`)
