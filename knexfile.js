ff6ad7279ce57d2b20ac9043312cbb213fc003d9
Greyson Elkins <greysonelkins@gmail.com>
2020-09-15 23:12:37 -0600

    Add config for production to knexfile

--- knexfile.js
+++ knexfile.js
@@ -8,40 +8,20 @@ module.exports = {
       directory: "./db/migrations",
     },
     seeds: {
-      directory: "./db/seeds/dev"
+      directory: "./db/seeds/dev",
     },
     useNullAsDefault: true,
   },
-
-  // staging: {
-  //   client: 'postgresql',
-  //   connection: {
-  //     database: 'my_db',
-  //     user:     'username',
-  //     password: 'password'
-  //   },
-  //   pool: {
-  //     min: 2,
-  //     max: 10
-  //   },
-  //   migrations: {
-  //     tableName: 'knex_migrations'
-  //   }
-  // },
-
-  // production: {
-  //   client: 'postgresql',
-  //   connection: {
-  //     database: 'my_db',
-  //     user:     'username',
-  //     password: 'password'
-  //   },
-  //   pool: {
-  //     min: 2,
-  //     max: 10
-  //   },
-  //   migrations: {
-  //     tableName: 'knex_migrations'
-  //   }
-  // }
+  
+  development: {
+    client: "pg",
+    connection: process.env.DATABASE_URL,
+    migrations: {
+      directory: __dirname + "/db/migrations",
+    },
+    seeds: {
+      directory: __dirname + "/db/seeds/dev",
+    },
+    useNullAsDefault: true,
+  },
 };
