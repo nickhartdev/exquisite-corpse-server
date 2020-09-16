b6fa8e74eaa80e5a13148cd473553ff49124fe11
Greyson Elkins <greysonelkins@gmail.com>
2020-09-16 00:47:02 -0600

    Change connection

--- server.js
+++ server.js
@@ -12,8 +12,8 @@ const {
 const app = express();
 const knex = require("knex")({
   client: "pg",
-  connection: `postgres://${process.env.DB_USER}:${process.env.DB_PASSWORD}@localhost/exquisite_data`
-  // connection: process.env.DATABASE_URL,
+  // connection: `postgres://${process.env.DB_USER}:${process.env.DB_PASSWORD}@localhost/exquisite_data`
+  connection: process.env.DATABASE_URL,
 });
 
 app.use(express.json());
