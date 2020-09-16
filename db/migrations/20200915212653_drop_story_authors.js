7d2ffb193b7ea07f580fd7e59653b624cb47bd56
Nick Hart <nick.hart.des@gmail.com>
2020-09-15 22:38:39 -0500

    Remove relational keys from first exports.down

--- db/migrations/20200915212653_drop_story_authors.js
+++ db/migrations/20200915212653_drop_story_authors.js
@@ -6,11 +6,5 @@ exports.up = function(knex) {
 
 exports.down = function(knex) {
   return knex.schema
-    .createTable('story_authors', function(table) {
-      table.integer("story_id");
-      table.foreign("story_id").references("stories.id");
-      table.integer("author_id");
-      table.foreign("author_id").references("authors.id");
-      table.timestamps(true, true);
-    });
+    .createTable('story_authors', function(table) {});
 };
