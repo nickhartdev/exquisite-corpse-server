d0c81c3bce1f230b4f1bc5e54800b37f17f908d3
Nick Hart <nick.hart.des@gmail.com>
2020-09-15 23:04:35 -0500

    Remove all export.downs

--- db/migrations/20200915212653_drop_story_authors.js
+++ db/migrations/20200915212653_drop_story_authors.js
@@ -5,12 +5,12 @@ exports.up = function(knex) {
 };
 
 exports.down = function(knex) {
-  return knex.schema
-    .createTable("story_authors", function (table) {
-      table.integer("story_id");
-      table.foreign("story_id").references("stories.id");
-      table.integer("author_id");
-      table.foreign("author_id").references("authors.id");
-      table.timestamps(true, true);
-    })
+  // return knex.schema
+  //   .createTable("story_authors", function (table) {
+  //     table.integer("story_id");
+  //     table.foreign("story_id").references("stories.id");
+  //     table.integer("author_id");
+  //     table.foreign("author_id").references("authors.id");
+  //     table.timestamps(true, true);
+  //   })
 };
