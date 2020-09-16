34fb0494d836ee50c719fdaa11385e000e0d0a20
Greyson Elkins <greysonelkins@gmail.com>
2020-09-15 23:41:12 -0600

    Add knexfile change

--- knexfile.js
+++ knexfile.js
@@ -12,7 +12,23 @@ module.exports = {
     },
     useNullAsDefault: true,
   },
-  
+
+  // staging: {
+  //   client: 'postgresql',
+  //   connection: {
+  //     database: 'my_db',
+  //     user:     'username',
+  //     password: 'password'
+  //   },
+  //   pool: {
+  //     min: 2,
+  //     max: 10
+  //   },
+  //   migrations: {
+  //     tableName: 'knex_migrations'
+  //   }
+  // },
+
   development: {
     client: "pg",
     connection: process.env.DATABASE_URL,
