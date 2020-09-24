344de79415dc8863de2905fccbe0d74ec156ba3d
Greyson Elkins <greysonelkins@gmail.com>
2020-09-24 12:27:31 -0600

    Spell out response

--- server.js
+++ server.js
@@ -18,8 +18,8 @@ const knex = require("knex")({
 
 app.use(express.json());
 app.use(cors());
-app.use(function (req, res) {
-  res.header("Access-Control-Allow-Origin", "exquisite-corpse-2005fe.herokuapp.com/"); // update to match the domain you will make the request from
+app.use(function (request, response) {
+  response.header("Access-Control-Allow-Origin", "exquisite-corpse-2005fe.herokuapp.com/"); // update to match the domain you will make the request from
   // res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
 })
 
