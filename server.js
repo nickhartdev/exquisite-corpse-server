184bc8bc7a6c92c4276249a8180a9716fb45f88e
Greyson Elkins <greysonelkins@gmail.com>
2020-09-24 12:47:14 -0600

    Add allow headers

--- server.js
+++ server.js
@@ -20,7 +20,7 @@ app.use(express.json());
 app.use(cors());
 app.use(function (request, response) {
   response.header("Access-Control-Allow-Origin", "exquisite-corpse-2005fe.herokuapp.com/"); // update to match the domain you will make the request from
-  // res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  response.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
 })
 
 app.set("port", process.env.PORT || 3005);
