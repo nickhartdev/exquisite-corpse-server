769904e5d5dd3c6670688250e7c2921a212f4eb2
Greyson Elkins <greysonelkins@gmail.com>
2020-09-28 16:07:21 -0600

    Remove comments

--- server.js
+++ server.js
@@ -19,16 +19,6 @@ const knex = require("knex")({
 
 app.use(express.json());
 app.use(cors());
-// app.use(function (req, res, next) {
-//   res.header("Access-Control-Allow-Origin", "*");
-//   res.header("Access-Control-Allow-Credentials", true);
-//   res.header("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE,OPTIONS");
-//   res.header(
-//     "Access-Control-Allow-Headers",
-//     "Origin,X-Requested-With,Content-Type,Accept,content-type,application/json"
-//   );
-//   next();
-// });
 
 app.set("port", process.env.PORT || 3005);
 app.locals.title = "The Exquisite Corpse server";
